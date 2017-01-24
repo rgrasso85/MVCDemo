591b93051b3bbdcc238f5ee2dcac640ab410ceef
rgrass2806 <rgrass2806@WS-AC-CTD-37001>
2017-01-24 09:44:12 -0500

    Add project files.

---
+++ MVCDemo/Controllers/PatientController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
